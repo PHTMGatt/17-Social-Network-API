4d39700314b3d6a79de360bdda6d959afabf15b1
PHTMGatt <phtmgatt@gmail.com>
2025-04-14 18:57:51 -0400

    ThoughtRoutes.ts rough draft

---
+++ src/routes/api/thoughtRoutes.ts
@@ -0,0 +1,29 @@
+// routes/api/thoughtRoutes.js
+const router = require('express').Router();
+const {
+  getThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/')
+  .get(getThoughts)
+  .post(createThought);
+
+// /api/thoughts/:id
+router.route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+  .post(addReaction)
+  .delete(removeReaction);
+
+module.exports = router;
