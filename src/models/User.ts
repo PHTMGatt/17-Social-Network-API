a7bdb8137f2b8e2e8675484a22f2bf29bc5806dd
PHTMGatt <phtmgatt@gmail.com>
2025-04-14 18:54:38 -0400

    User.ts rough draft

---
+++ src/models/User.ts
@@ -0,0 +1,47 @@
+// models/User.js
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // Simple regex for email validation
+      match: [/.+@.+\..+/, 'Please enter a valid email address'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property 'friendCount'
+UserSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+module.exports = User;
